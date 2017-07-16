0051c1c224973182216b1ee9f6c863bb187b3339
Joher <jmmc_cortez@outlook.com>
2017-07-16 16:31:19 -0500

    se corrigio config.php

--- config.php
+++ config.php
@@ -5,13 +5,4 @@
 		'usuario' => 'root',
 		'pass' => '12345678'
 	);
-
-<<<<<<< HEAD
-=======
-	// $bd_usuario = array(
-	// 	'usuario' => ,
-	// 	'contraseña' =>
-	// );
-
->>>>>>> 36070cb3a524d60a7cc86dba143943828416c350
  ?>
